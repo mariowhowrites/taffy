231ef398ff0608d4afbf86210136596ff3333a5a
Mario Vega <mario@ceibaweb.com>
2017-10-20 10:52:04 -0700

    dump oauth id

---
+++ site/helpers/Tags.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Statamic\SiteHelpers;
+
+use Statamic\API\User;
+use Statamic\Extend\Tags as AbstractTags;
+
+class Tags extends AbstractTags
+{
+    /**
+     * The {{ site:example }} tag
+     *
+     * @return mixed
+     */
+    public function example()
+    {
+        //
+    }
+
+    public function showSpotifyId() {
+        $user == User::getCurrent();
+
+        return $user->getOAuthID('spotify');
+    }
+}
